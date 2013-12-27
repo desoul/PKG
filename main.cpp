68829a6d6afd03c6ab2e17dbc738ea7e7a30678d
desoul <mr.pankivskii@list.ru>
2013-12-27 21:40:15 +0200

    done - 33,3%

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@
 	//поверхня під столиком
 	dc.Rectangle(r/15,b-b/40,r-r/15,b-b/25);
 	//кулька
-	dc.MoveTo(r/2+(b/50+r/50),b/6);
-	dc.AngleArc(r/2,b/6,b/50+r/50,0,360);
+	dc.MoveTo(r/2+(b/400+r/400),b/6);
+	dc.AngleArc(r/2,b/6,b/400+r/400,0,360);
 }
