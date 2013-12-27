ada1431b817fe1a6bb1ecf8be2ab4769be1c4e03
desoul <mr.pankivskii@list.ru>
2013-12-27 10:31:00 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+double r,b,l,t;
+	CRect v;
+	GetClientRect (&v);
+	r=v.right;
+	b=v.bottom;
+	l=v.left;
+	t=v.top;
+	//поверхня під столиком
+	dc.MoveTo(l+50,b-20);
+	dc.LineTo(r-50,b-20);
+	dc.LineTo(r-50,b-40);
+	dc.LineTo(l+50,b-40);
+	dc.LineTo(l+50,b-20);
+	//кулька
+	dc.MoveTo(r/2+15,t+100);
+	dc.AngleArc(r/2,t+100,15,0,360);
